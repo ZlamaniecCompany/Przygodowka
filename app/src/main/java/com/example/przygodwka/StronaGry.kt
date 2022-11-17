ba43da0d5a84dd59b6b2a68952fb34173eab5c4c
Błotnicki <zlamanieccompany@gmail.com>
2022-11-17 23:00:57 +0100

    1StronaGry

---
+++ app/src/main/java/com/example/przygodwka/StronaGry.kt
@@ -0,0 +1,11 @@
+package com.example.przygodwka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StronaGry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_strona_gry)
+    }
+}
