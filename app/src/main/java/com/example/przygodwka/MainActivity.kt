fbe0f96dc2e5d6897646ef340fe62e29520882f5
Błotnicki <zlamanieccompany@gmail.com>
2022-11-17 23:40:44 +0100

    Przejście do drugiej aktywności, dodanie dependencies

--- app/src/main/java/com/example/przygodwka/MainActivity.kt
+++ app/src/main/java/com/example/przygodwka/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.przygodwka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener{
+
+            val Intent = Intent(this, StronaGry:: class.java)
+            startActivity(Intent)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
